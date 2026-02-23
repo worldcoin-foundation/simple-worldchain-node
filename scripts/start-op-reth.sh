57b201fd3afcafaa933c4606e5108ee7aa11c83a
0xPenryn <29718876+0xPenryn@users.noreply.github.com>
2026-02-23 17:17:20 -0500

    feat: reth snapshot downloading

--- scripts/start-op-reth.sh
+++ scripts/start-op-reth.sh
@@ -8,6 +8,31 @@ else
   export CHAIN_NAME="$NETWORK_NAME"
 fi
 
+# download snapshot if datadir is empty
+if [ -z "$( ls -A '/data' )" ] && [ "$( uname -m )" = "x86_64" ]; then
+  if [ "$NETWORK_NAME" = "worldchain-mainnet" ]; then
+    export BUCKET="world-chain-snapshots"
+  else
+    export BUCKET="world-chain-testnet-snapshots"
+  fi
+  if [ "$NODE_TYPE" = "full" ]; then
+    export FILE_NAME="reth_full.tar.lz4"
+  else
+    export FILE_NAME="reth_archive.tar.lz4"
+  fi
+
+  cd /data
+  echo "Downloading $NETWORK_NAME $NODE_TYPE snapshot, please be patient!"
+  aws s3 cp --no-sign-request s3://${BUCKET}/${FILE_NAME} "${FILE_NAME}"
+  echo "Snapshot downloaded, extracting..."
+  tar --use-compress-program="lz4 -d" -xf "${FILE_NAME}"
+  mv /data/reth/* /data && rm -r /data/reth "${FILE_NAME}"
+  echo "Snapshot downloadeded and extracted! Restarting container to proceed to sync."
+  exit 0
+else
+  echo "Skipping snapshot download due to existing datadir or non-x86 CPU architecture"
+fi
+
 exec world-chain node \
   --datadir=/data \
   --ws \
