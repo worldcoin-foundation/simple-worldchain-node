2391e688d52547e79f37abdaa1bf37d7a6d7050d
0xPenryn <29718876+0xPenryn@users.noreply.github.com>
2026-03-27 14:53:29 -0400

    remove x86_64 check

--- scripts/start-op-reth.sh
+++ scripts/start-op-reth.sh
@@ -13,9 +13,6 @@ if [ "$DOWNLOAD_SNAPSHOT" = "false" ]; then
   echo "Snapshot download disabled"
 elif [ -f "/data/reth.toml" ]; then
   echo "Skipping snapshot download due to existing datadir"
-elif [ "$( uname -m )" != "x86_64" ]; then
-  echo "Skipping snapshot download due to incompatible CPU architecture"
-  echo "Run `export DOCKER_DEFAULT_PLATFORM=linux/amd64` before `docker compose up` to switch to a compatible mode for this terminal session"
 else
   if [ "$NETWORK_NAME" = "worldchain-mainnet" ]; then
     export BUCKET="world-chain-snapshots"
