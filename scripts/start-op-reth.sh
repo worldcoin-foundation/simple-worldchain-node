e666ddc8a3ca20103fcb32ef8c173852c92fe2f6
0xPenryn <29718876+0xPenryn@users.noreply.github.com>
2026-03-27 14:53:14 -0400

    smart dl method based on disk space

--- scripts/start-op-reth.sh
+++ scripts/start-op-reth.sh
@@ -29,11 +29,25 @@ else
   fi
 
   cd /data
-  echo "Downloading $NETWORK_NAME $NODE_TYPE snapshot, please be patient!"
-  aws s3 cp --region eu-central-2 --no-sign-request s3://${BUCKET}/${FILE_NAME} "${FILE_NAME}"
-  echo "Snapshot downloaded, extracting..."
-  tar --use-compress-program="lz4 -d" -xf "${FILE_NAME}"
-  mv /data/reth/* /data && rm -r /data/reth "${FILE_NAME}"
+  DL_SIZE=$(curl -sI https://${BUCKET}.s3.eu-central-2.amazonaws.com/${FILE_NAME} | awk -F': ' '/Content-Length/{print $2}' | tr -d '\r')
+  AVAIL_SPACE=$(df -B 1 --output=avail /data | tail -n 1)
+  if (( $AVAIL_SPACE < ( $DL_SIZE * 3 / 2 ) )); then
+    echo "Aborting snapshot snapshot download due to limited disk space"
+    echo "Estimated disk space required: $( numfmt --to=iec $(( $DL_SIZE * 3 / 2 )) )"
+    echo "Available disk space: $( numfmt --to=iec $AVAIL_SPACE )"
+    exit 1
+  elif (( $AVAIL_SPACE > ( $DL_SIZE * 5 / 2 ) )); then
+    echo "Downloading $NETWORK_NAME $NODE_TYPE snapshot, please be patient!"
+    apt update -qq > /dev/null 2>&1
+    apt install -y -qq --no-install-recommends aria2 > /dev/null 2>&1
+    aria2c --file-allocation=falloc --show-console-readout false --download-result=hide --summary-interval=5 -x 16 -s 16 https://${BUCKET}.s3.eu-central-2.amazonaws.com/${FILE_NAME}
+    echo "Snapshot downloaded, extracting..."
+    tar --strip-components=1 -I lz4 -xvf "${FILE_NAME}"
+    rm "${FILE_NAME}"
+  else
+    echo "Downloading $NETWORK_NAME $NODE_TYPE snapshot with streamed decompression due to limited disk space, please be patient!"
+    curl https://${BUCKET}.s3.eu-central-2.amazonaws.com/${FILE_NAME} | lz4 -dc | tar --strip-components=1 -xvf - 
+  fi
   echo "Snapshot downloaded and extracted! Restarting container to proceed to sync."
   exit 0
 fi
