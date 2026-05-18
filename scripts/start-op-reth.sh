4d2104548444c164c93d6d9871eccb7acd6e9e8c
0xPenryn <29718876+0xPenryn@users.noreply.github.com>
2026-05-18 12:13:10 -0700

    v2.2.0-1 (#18)
    * update world-chain
    
    * remove unneeded op-node flags
    
    * simpler snapshot downloads
    
    * remove unused utils
    
    * op-node v1.18.0, remove jovian override
    
    * op-geth v1.101702.2, remove jovian override
    
    * use named chain for world-chain client
    
    * cleanup
    
    * quiet snapshot logs

--- scripts/start-op-reth.sh
+++ scripts/start-op-reth.sh
@@ -28,22 +28,14 @@ else
   cd /data
   DL_SIZE=$(curl -sI https://${BUCKET}.s3.eu-central-2.amazonaws.com/${FILE_NAME} | awk -F': ' '/Content-Length/{print $2}' | tr -d '\r')
   AVAIL_SPACE=$(df -B 1 --output=avail /data | tail -n 1)
-  if (( $AVAIL_SPACE < ( $DL_SIZE * 3 / 2 ) )); then
+  if (( $AVAIL_SPACE < $DL_SIZE )); then
     echo "Aborting snapshot snapshot download due to limited disk space"
-    echo "Estimated disk space required: $( numfmt --to=iec $(( $DL_SIZE * 3 / 2 )) )"
+    echo "Compressed snapshot size: $( numfmt --to=iec $DL_SIZE )"
     echo "Available disk space: $( numfmt --to=iec $AVAIL_SPACE )"
     exit 1
-  elif (( $AVAIL_SPACE > ( $DL_SIZE * 5 / 2 ) )); then
-    echo "Downloading $NETWORK_NAME $NODE_TYPE snapshot, please be patient!"
-    apt update -qq > /dev/null 2>&1
-    apt install -y -qq --no-install-recommends aria2 > /dev/null 2>&1
-    aria2c --file-allocation=falloc --show-console-readout false --download-result=hide --summary-interval=5 -x 16 -s 16 https://${BUCKET}.s3.eu-central-2.amazonaws.com/${FILE_NAME}
-    echo "Snapshot downloaded, extracting..."
-    tar --strip-components=1 -I lz4 -xvf "${FILE_NAME}"
-    rm "${FILE_NAME}"
   else
-    echo "Downloading $NETWORK_NAME $NODE_TYPE snapshot with streamed decompression due to limited disk space, please be patient!"
-    curl https://${BUCKET}.s3.eu-central-2.amazonaws.com/${FILE_NAME} | lz4 -dc | tar --strip-components=1 -xvf - 
+    echo "Downloading $NETWORK_NAME $NODE_TYPE snapshot, please be patient!"
+    s3fcp http https://${BUCKET}.s3.eu-central-2.amazonaws.com/${FILE_NAME} | lz4 -dc | tar --strip-components=1 -xf -
   fi
   echo "Snapshot downloaded and extracted! Restarting container to proceed to sync."
   exit 0
@@ -65,7 +57,7 @@ exec world-chain node \
   --authrpc.port=8551 \
   --authrpc.jwtsecret=/shared/jwt.txt \
   --metrics=0.0.0.0:6060 \
-  --chain="/chainconfig/genesis.json" \
+  --chain=${CHAIN_NAME} \
   --rollup.sequencer-http=https://${NETWORK_NAME}-sequencer.g.alchemy.com \
   --rollup.disable-tx-pool-gossip \
   --port="${PORT__EXECUTION_P2P:-30303}" \
